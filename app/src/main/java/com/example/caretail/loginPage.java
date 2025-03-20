30307c3682d60f07b14d99d7d36f9b4994f4dee8
Lav-18 <lavanyashah708@gmail.com>
2025-03-20 09:09:59 +0530

    first commit

--- app/src/main/java/com/example/caretail/loginPage.java
+++ app/src/main/java/com/example/caretail/loginPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class splashScreenn extends AppCompatActivity {
+public class loginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screenn);
+        setContentView(R.layout.activity_login_page);
     }
 }
